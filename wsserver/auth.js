d6d41cf4366740bce9b1f2c96f635cf2967d2e81
FilippoCimonetti <145663509+FilippoCimonetti@users.noreply.github.com>
2024-11-25 15:15:54 +0000

    bbbb

--- wsserver/auth.js
+++ wsserver/auth.js
@@ -1,2 +1,18 @@
 const jwt  = require('jsonwebtoken');
 const config = require('./require');
+const pool = require('./db');
+
+function adminAuth(request, response, next){
+    //Leggo il nome nel campo authorization
+    const authHeader = request.headers['authorization'];
+    //Se esiste authHeader, lo splitto [essendo una stringa] tramite spaziatura e prendo il secondo valore
+    //Il valore saràroba del tipo: Bearer JSDHWINONSCOJM...
+    const token = authHeader && authHeader.split(' ').[1];
+    
+    if (!token){
+        return response.status(401).json({
+            messaggio: 'Accesso negato.'
+        })
+    }
+    
+}
