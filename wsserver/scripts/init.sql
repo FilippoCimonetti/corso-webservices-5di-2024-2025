805051bf94273a1aac6c1acdc430d468c03328d5
FilippoCimonetti <145663509+FilippoCimonetti@users.noreply.github.com>
2025-01-13 14:57:11 +0000

    aiut2

--- wsserver/scripts/init.sql
+++ wsserver/scripts/init.sql
@@ -18,10 +18,23 @@ CREATE TABLE users (
     telefono VARCHAR(20),
     cell VARCHAR(20),
     mail VARCHAR(100),
-    username VARCHAR(20) NOT NULL UNIQUE+
-    ,
+    username VARCHAR(20) NOT NULL UNIQUE,
     password VARCHAR(200) NOT NULL,
     ruolo VARCHAR(13) NOT NULL
+);
+
+CREATE TABLE prodotti(
+    marca VARCHAR(20) NOT NULL,
+    barcode VARCHAR(10) PRIMARY KEY NOT NULL,
+    prezzo DECIMAL (3,2) NOT NULL,
+    tipo VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE recensioni(
+    valutazione NUMERIC(1) NOT NULL,
+    user VARCHAR(20),
+    descrizione VARCHAR(100) NOT NULL,
+    prodotto VARCHAR(20) REFERENCES prodotti(barcode) 
 );
 
 CREATE TABLE logs (
